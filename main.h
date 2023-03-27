be4b73ae557f5d814145c99c3ba92ec881ba36fd
Sauceboy7 <adebiyifavour@7gmail.com>
2023-03-27 14:43:50 -0700

    task 1

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _putstr(const char *String);
+int print_int(int n);
+int _printf(const char *format, ...);
+
+#endif
