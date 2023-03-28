06470e7b5535495fd8924788e0109cd2ff558a46
Sauceboy7 <adebiyifavour@7gmail.com>
2023-03-28 15:31:01 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -2,51 +2,50 @@
 #include "main.h"
 
 /**
- * _printf - prints a formatted string
- * @format: string to be printed
- * Return: number of printed characters
- */
-
+* _printf - prints a formatted string
+* @format: string to be printed
+* Return: number of printed characters, or -1 on error
+*/
 int _printf(const char *format, ...)
 {
-	int i, size;
-	va_list args;
+int i, size = 0;
+va_list args;
 
-	va_start(args, format);
-	i = 0, size = 0;
-	while (*(format + i) != '\0')
-	{
-		if (*(format + i) == '%')
-		{
-			switch (*(format + i + 1))
-			{
-				case 'd':
-				case 'i':
-					size +=  print_int(va_arg(args, int));
-					break;
-				case 'c':
-					_putchar(va_arg(args, int));
-					size++;
-					break;
-				case 's':
-					size += _putstr(va_arg(args, char*));
-					break;
-				case '%':
-					_putchar('%');
-					size++;
-					break;
-				default:
-					return (-1);
-			}
-		i += 2;
-		}
-		else
-		{
-			_putchar(*(format + i));
-			size++, i++;
-		}
-	}
-	va_end(args);
-	return (size);
+va_start(args, format);
+i = 0, size = 0;
+while (format && *(format + i) != '\0')
+{
+if (*(format + i) == '%')
+{
+if (*(format + i + 1) == '\0')
+return (-1);
+switch (*(format + i + 1))
+{
+case 'd':
+case 'i':
+size += print_int(va_arg(args, int));
+break;
+case 'c':
+_putchar(va_arg(args, int));
+size++;
+break;
+case 's':
+size += _putstr(va_arg(args, char *));
+break;
+case '%':
+_putchar('%');
+size++, break;
+default:
+return (-1);
 }
-
+i += 2;
+}
+else
+{
+_putchar(*(format + i));
+size++, i++;
+}
+}
+va_end(args);
+return (size);
+}
