eedd77c8d2c6b8e60e026f3f7f6d1c0c09b338eb
VictorMaxima <victormaxchidera@gmail.com>
2023-03-28 16:17:17 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -12,6 +12,8 @@ int i, size = 0;
 va_list args;
 
 va_start(args, format);
+if (!format)
+return (-1);
 while (format && *(format + i) != '\0')
 {
 if (*(format + i) == '%')
@@ -25,15 +27,13 @@ case 'i':
 size += print_int(va_arg(args, int));
 break;
 case 'c':
-_putchar(va_arg(args, int));
-size++;
+_putchar(va_arg(args, int)), size++;
 break;
 case 's':
 size += _putstr(va_arg(args, char *));
 break;
 case '%':
-_putchar('%');
-size++;
+_putchar('%'), size++;
 break;
 default:
 return (-1);
